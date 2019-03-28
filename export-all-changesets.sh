2f0c1d286b1c27645daa350eb3dd873c5d934708
Andrew Janke <floss@apjanke.net>
2019-03-28 03:04:58 -0400

    incremental exports for export-all-changesets

--- export-all-changesets.sh
+++ export-all-changesets.sh
@@ -5,14 +5,14 @@
 #
 # Issue: This only gets changesets on the main branch. Are there other branches
 # that we need to export?
-#
-# TODO: Should modify this to do incremental update for just changesets that
-# have not already been exported.
 
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'changesets=($(cat original-hg-changesets | sort -n))'
 
 for cs in "${changesets[@]}"; do
-	hg -R octave export $cs > changeset-exports/${cs}.patch
+	cs_file="changeset-exports/${cs}.patch"
+	if [[ ! -f "$cs_file" ]]; then
+		hg -R octave export $cs > "$cs_file"
+	fi
 done
 
 echo "all changesets exported"
